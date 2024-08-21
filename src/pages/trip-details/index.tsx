41e2db49b7e3c46be23021c3d217c2f9bcfa7259
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-21 17:05:00 -0300

    added important-links and guests tsx

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,16 +1,8 @@
-import {
-  MapPin,
-  Calendar,
-  Settings2,
-  Plus,
-  CircleCheck,
-  Link2,
-  UserCog,
-  CircleDashed,
-} from "lucide-react";
+import { MapPin, Calendar, Settings2, Plus, CircleCheck } from "lucide-react";
 
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
+import { ImportantLinks } from "./important-links";
 
 export function TripDetailsPage() {
   const [isCreateAcivityModalOpen, setIsCreateActivityModalOpen] =
@@ -99,75 +91,8 @@ export function TripDetailsPage() {
         </div>
 
         <div className="w-80 space-y-6">
-          <div className="space-y-6">
-            <h2 className="font-semibold text-xl">Links Importantes</h2>
-            <div className="space-y-5">
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5 ">
-                  <span className="block font-medium text-zinc-100">
-                    Reserva do AirBnB
-                  </span>
-                  <a
-                    href="#"
-                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-                  >
-                    https://www.airbnb.com.br/rooms/104700011
-                  </a>
-                </div>
-                <Link2 className="text-zinc-400 size-5 shrink-0" />
-              </div>
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5 ">
-                  <span className="block font-medium text-zinc-100">
-                    Reserva do AirBnB
-                  </span>
-                  <a
-                    href="#"
-                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-                  >
-                    https://www.airbnb.com.br/rooms/104700011
-                  </a>
-                </div>
-                <Link2 className="text-zinc-400 size-5 shrink-0" />
-              </div>
-            </div>
-            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
-              <Plus className="size-5" />
-              Cadastrar novo link
-            </button>
-          </div>
+          <ImportantLinks />
           <div className="w-full h-px bg-zinc-800" />
-          <div className="space-y-6">
-            <h2 className="font-semibold text-xl">Convidados</h2>
-            <div className="space-y-5">
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5 ">
-                  <span className="block font-medium text-zinc-100">
-                    Jessica White
-                  </span>
-                  <span className="block text-sm text-zinc-400 truncate">
-                    jessica.white44@yahoo.com
-                  </span>
-                </div>
-                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-              </div>
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5 ">
-                  <span className="block font-medium text-zinc-100">
-                    Dr. Rita Pacocha
-                  </span>
-                  <span className="block text-sm text-zinc-400 truncate">
-                    lacy.stiedemann@gmail.com
-                  </span>
-                </div>
-                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-              </div>
-            </div>
-            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
-              <UserCog className="size-5" />
-              Gerenciar Convidados
-            </button>
-          </div>
         </div>
       </main>
 
