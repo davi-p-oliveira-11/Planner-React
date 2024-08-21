f73e701e3c4f77698db98b20d9973923e0d5e039
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-08-21 17:13:12 -0300

    added activities file

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,8 +1,10 @@
-import { MapPin, Calendar, Settings2, Plus, CircleCheck } from "lucide-react";
+import { MapPin, Calendar, Settings2, Plus } from "lucide-react";
 
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./important-links";
+import { Guests } from "./guests";
+import { Activities } from "./activities";
 
 export function TripDetailsPage() {
   const [isCreateAcivityModalOpen, setIsCreateActivityModalOpen] =
@@ -52,47 +54,13 @@ export function TripDetailsPage() {
             </button>
           </div>
 
-          <div className="space-y-8">
-            <div className="space-y-2.5">
-              <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">
-                  Dia 17
-                </span>
-                <span className="text-xs text-zinc-500">Sábado</span>
-              </div>
-              <p className="text-zinc-500 text-sm">
-                Nenhuma atividade cadastrada nessa data.
-              </p>
-            </div>
-
-            <div className="space-y-2.5">
-              <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">
-                  Dia 18
-                </span>
-                <span className="text-xs text-zinc-500">Domingo</span>
-              </div>
-              <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                  <CircleCheck className="size-5 text-lime-300" />
-                  <span className="text-zinc-100">Academia em grupo</span>
-                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
-                </div>
-              </div>
-              <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                  <CircleCheck className="size-5 text-lime-300" />
-                  <span className="text-zinc-100">Academia em grupo</span>
-                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Activities />
         </div>
 
         <div className="w-80 space-y-6">
           <ImportantLinks />
           <div className="w-full h-px bg-zinc-800" />
+          <Guests />
         </div>
       </main>
 
